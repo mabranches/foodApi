65d5ad0056687e90bff2eb715819b272f154aedb
Manoel Rebelo abranches <abranches@gmail.com>
2021-03-27 23:59:12 -0300

    First version of recipe controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,4 +1,28 @@
 # 'sinatra'
-get '/search' do
-  "SEARCH"
+require './app/cache/in_memory_cache'
+
+get '/search' do |query|
+  recipe_service.search(query)
+end
+
+private
+
+def recipe_service
+  @recipe_service ||= RecipeService.new(cache_service, recipe_source)
+end
+
+def cache_service
+  @cache_service ||= CacheService.ne(underlying_cache)
+end
+
+def recipe_source
+  @recipe_source ||= RecipeSource.new(underlying_source)
+end
+
+def underlying_cache
+  @underlying_cache ||= InMemoryCache.new
+end
+
+def underlying_source
+  @underlying_source ||= RecipePuppyClient
 end
